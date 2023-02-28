1f5ae8bb7c0bafc665709d6b633fb129d604642f
alanbeals <alan@finventures.ca>
2023-02-28 14:29:18 -0500

    dao total income

--- src/main/java/dao/AgeDao.java
+++ src/main/java/dao/AgeDao.java
@@ -1,35 +1,80 @@
 package dao;
 import bean.Age;
+import bean.TotalIncome;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AgeDao {
-    public int registerAge (Age emp) throws ClassNotFoundException {
-        String INSERT_USERS_SQL = "INSERT INTO age" +
-                "(id, ageGroup, censusYear, geographicArea, combined, male, female) VALUES " +
-                "(?, ?, ?, ?, ?, ?, ?);";
-        int result = 0;
-
-        Class.forName("com.mysql.jdbc.Driver");
-        // Get Connection
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root", "MyTr0ubl3M@ker");
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-            preparedStatement.setInt(1,1);
-            preparedStatement.setString(2, emp.getAgeGroup());
-            preparedStatement.setString(3, emp.getCensusYear());
-            preparedStatement.setString(4, emp.getGeographicArea());
-            preparedStatement.setString(5, emp.getCombined());
-            preparedStatement.setString(6, emp.getMale());
-            preparedStatement.setString(7, emp.getFemale());
-
-            result = preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-        return result;
-    }
+//    public static final String UPDATE = "INSERT INTO user"+
+//            "(ageId,ageGroup, censusYear, geographicArea, combined, male, female) VALUES"+
+//            "(?,?,?,?,?,?,?);";
+//    public static final String GET = "SELECT * FROM user "+
+//            "WHERE id=?;";
+//    public static final String GET_ALL = "SELECT * FROM user;";
+//    public static final String DELETE = "DELETE from user "+
+//            "WHERE id=?;";
+//    public static Connection connection;
+//
+//    public AgeDao() throws ClassNotFoundException, SQLException {
+//        Class.forName("com.mysql.jdbc.Driver");
+//        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/census","root","Test12@");
+//    }
+//
+//    public static int update(Age age) {
+//        int result=0;
+//        try{
+//            PreparedStatement preparedStatement=connection.prepareStatement(UPDATE);
+//
+//            preparedStatement.setInt(1, totalIncome.getId());
+//            preparedStatement.setString(2,totalIncome.getDescription());
+//            result=preparedStatement.executeUpdate();
+//        }
+//        catch (SQLException exception){
+//            exception.printStackTrace();
+//        }
+//        return result;
+//    }
+//
+//    public static ResultSet get(Integer id) {
+//
+//        ResultSet result=null;
+//        try
+//        {
+//            PreparedStatement preparedStatement=connection.prepareStatement(GET);
+//            preparedStatement.setInt(1,id);
+//            result=preparedStatement.executeQuery();
+//        }
+//        catch (SQLException exception){
+//            exception.printStackTrace();
+//        }
+//        return result;
+//    }
+//
+//    public static ResultSet getAll() {
+//
+//        ResultSet result=null;
+//        try
+//        {
+//            PreparedStatement preparedStatement=connection.prepareStatement(GET_ALL);
+//            result=preparedStatement.executeQuery();
+//        }
+//        catch (SQLException exception){
+//            exception.printStackTrace();
+//        }
+//        return result;
+//    }
+//
+//    public static void delete(Integer id){
+//        int result=0;
+//        try{
+//            PreparedStatement preparedStatement=connection.prepareStatement(DELETE);
+//
+//            preparedStatement.setInt(1, id);
+//            result=preparedStatement.executeUpdate();
+//        }
+//        catch (SQLException exception){
+//            exception.printStackTrace();
+//        }
+//        return result;
+//    }
 }
--- src/main/java/dao/TotalIncomeDao.java
+++ src/main/java/dao/TotalIncomeDao.java
@@ -1,4 +1,81 @@
 package dao;
 
+import bean.TotalIncome;
+
+import java.sql.*;
+
+
 public class TotalIncomeDao {
+    public static final String UPDATE = "INSERT INTO user"+
+            "(id,description) VALUES"+
+            "(?,?);";
+    public static final String GET = "SELECT * FROM user "+
+            "WHERE id=?;";
+    public static final String GET_ALL = "SELECT * FROM user;";
+    public static final String DELETE = "DELETE from user "+
+            "WHERE id=?;";
+    public static Connection connection;
+
+    public TotalIncomeDao() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/census","root","Test12@");
+    }
+
+    public static int update(TotalIncome totalIncome) {
+        int result=0;
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement(UPDATE);
+
+            preparedStatement.setInt(1, totalIncome.getId());
+            preparedStatement.setString(2,totalIncome.getDescription());
+            result=preparedStatement.executeUpdate();
+        }
+        catch (SQLException exception){
+            exception.printStackTrace();
+        }
+        return result;
+    }
+
+    public static ResultSet get(Integer id) {
+
+        ResultSet result=null;
+        try
+        {
+            PreparedStatement preparedStatement=connection.prepareStatement(GET);
+            preparedStatement.setInt(1,id);
+            result=preparedStatement.executeQuery();
+        }
+        catch (SQLException exception){
+            exception.printStackTrace();
+        }
+        return result;
+    }
+
+    public static ResultSet getAll() {
+
+        ResultSet result=null;
+        try
+        {
+            PreparedStatement preparedStatement=connection.prepareStatement(GET_ALL);
+            result=preparedStatement.executeQuery();
+        }
+        catch (SQLException exception){
+            exception.printStackTrace();
+        }
+        return result;
+    }
+
+    public static int delete(Integer id){
+        int result=0;
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement(DELETE);
+
+            preparedStatement.setInt(1, id);
+            result=preparedStatement.executeUpdate();
+        }
+        catch (SQLException exception){
+            exception.printStackTrace();
+        }
+        return result;
+    }
 }
